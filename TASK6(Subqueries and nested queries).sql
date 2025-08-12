94fb7fd408c188c4b64b5e1eb933862c82fb5a33
shiwangisingh552 <shiwangisingh552@gmail.com>
2025-08-12 21:49:57 +0530

    TASK6(Subqueries and nested queries).sql

---
+++ TASK6(Subqueries and nested queries).sql
@@ -0,0 +1,15 @@
+1.CREATE Customers table:
+
+    CREATE TABLE Customers (
+        CustomerID INTEGER PRIMARY KEY,
+        Name TEXT,
+        City TEXT,
+    );
+
+
+--Insert into Customers:
+
+    INSERT INTO Customers (CustomerID, Name, City) VALUES(1, 'Alice', 'Delhi');
+    INSERT INTO Customers (CustomerID, Name, City) VALUES(2, 'Bob', 'Mumbai');
+    INSERT INTO Customers (CustomerID, Name, City) VALUES(3, 'Charlie', 'Chennai');
+    INSERT INTO Customers (CustomerID, Name, City) VALUES(4, 'Diana', 'Kolkata');
